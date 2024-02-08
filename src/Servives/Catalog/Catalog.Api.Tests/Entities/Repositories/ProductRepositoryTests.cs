7d40d7e577d77bcd534772bdbdbdccd804d92aae
Maryna <marinasuboch@gmail.com>
2024-02-08 13:06:37 +0100

    Add CatalogApi Test Project

---
+++ src/Servives/Catalog/Catalog.Api.Tests/Entities/Repositories/ProductRepositoryTests.cs
@@ -0,0 +1,48 @@
+﻿namespace Catalog.API.Entities.Repositories.Tests
+{
+    [TestFixture()]
+    public class ProductRepositoryTests
+    {
+        [Test()]
+        public void CreateProductAsyncTest()
+        {
+
+        }
+
+        [Test()]
+        public void DeleteProductAsyncTest()
+        {
+
+        }
+
+        [Test()]
+        public void GetProductByCategoryAsyncTest()
+        {
+
+        }
+
+        [Test()]
+        public void GetProductByConsultantAsyncTest()
+        {
+
+        }
+
+        [Test()]
+        public void GetProductByIdAsyncTest()
+        {
+
+        }
+
+        [Test()]
+        public void GetProductsAsyncTest()
+        {
+
+        }
+
+        [Test()]
+        public void UpdateProductAsyncTest()
+        {
+
+        }
+    }
+}
